4b88ebf49d39135e920c8642caccdaf22a81e3ee
netfloww <wreosh3@gmail.com>
2026-09-10 16:39:32 +0300

    Create books.js

---
+++ books.js
@@ -0,0 +1,9 @@
+const booksData = [
+    {
+        title: "العادات الذرية",
+        category: "تنمية بشرية",
+        desc: "كتاب يقدم منهجاً عملياً وبسيطاً لبناء عادات حسنة والتخلص من العادات السيئة خطوة بخطوة.",
+        cover: "images/atomic-habits.jpg",
+        pdfUrl: "pdfs/atomic-habits.pdf"
+    }
+];
